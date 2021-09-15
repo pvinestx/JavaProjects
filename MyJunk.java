3701371e626221a46905f37db09cc67f498545c0
pvinestx <90805405+pvinestx@users.noreply.github.com>
2021-09-15 18:42:29 -0500

    Update MyJunk.java

--- MyJunk.java
+++ MyJunk.java
@@ -4,7 +4,8 @@ public class MyJunk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("This is my junk!");
+		System.out.println("This is my junk!");
+		System.out.println("This is your junk...");
 	}
 
 }
